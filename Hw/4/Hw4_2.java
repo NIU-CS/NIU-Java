ea02828c1d903583b389daf705c121f9d02e10e4
young <young20050727@gmail.com>
2023-10-25 16:45:38 +0800

    update

---
+++ Hw/4/Hw4_2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Hw4_2 {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int a,b,c;
+        a = in.nextInt();
+        b = in.nextInt();
+        c = in.nextInt();
+        System.out.println(a+b>c && a+c>b && b+c>a);
+    }
+}
---
+++ Hw/4/Hw4_3.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Hw4_3 {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(n < 1000 && n >= 100);
+    }
+}
