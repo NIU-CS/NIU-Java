a2a7206358ead674a7d87f4138e12b85c5ce4ed5
young <young20050727@gmail.com>
2023-12-02 22:38:53 +0800

    update

---
+++ Hw/6/Hw6_1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Hw6_1 {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int x1 = in.nextInt();
+        int y1 = in.nextInt();
+        int x2 = in.nextInt();
+        int y2 = in.nextInt();
+        double distance = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+        System.out.printf("%.2f\n", distance);
+    }
+}
---
+++ Hw/6/Hw6_2.java
@@ -0,0 +1,109 @@
+import java.util.Scanner;
+
+public class Hw6_2 {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int month = in.nextInt();
+        int day = in.nextInt();
+
+        if (month == 1) {
+            if (day >= 1 && day <= 19) {
+                System.out.println("魔羯");
+            } else if (day >= 20 && day <= 31) {
+                System.out.println("水瓶");
+            } else {
+                System.out.println("Invalid");
+            }
+        } else if (month == 2) {
+            if (day >= 1 && day <= 18) {
+                System.out.println("水瓶");
+            } else if (day >= 19 && day <= 29) {
+                System.out.println("雙魚");
+            } else {
+                System.out.println("Invalid");
+            }
+        } else if (month == 3) {
+            if (day >= 1 && day <= 20) {
+                System.out.println("雙魚");
+            } else if (day >= 21 && day <= 31) {
+                System.out.println("牡羊");
+            } else {
+                System.out.println("Invalid");
+            }
+        } else if (month == 4) {
+            if (day >= 1 && day <= 19) {
+                System.out.println("牡羊");
+            } else if (day >= 20 && day <= 30) {
+                System.out.println("金牛");
+            } else {
+                System.out.println("Invalid");
+            }
+        } else if (month == 5) {
+            if (day >= 1 && day <= 20) {
+                System.out.println("金牛");
+            } else if (day >= 21 && day <= 31) {
+                System.out.println("雙子");
+            } else {
+                System.out.println("Invalid");
+            }
+        } else if (month == 6) {
+            if (day >= 1 && day <= 21) {
+                System.out.println("雙子");
+            } else if (day >= 22 && day <= 30) {
+                System.out.println("巨蟹");
+            } else {
+                System.out.println("Invalid");
+            }
+        } else if (month == 7) {
+            if (day >= 1 && day <= 22) {
+                System.out.println("巨蟹");
+            } else if (day >= 23 && day <= 31) {
+                System.out.println("獅子");
+            } else {
+                System.out.println("Invalid");
+            }
+        } else if (month == 8) {
+            if (day >= 1 && day <= 22) {
+                System.out.println("獅子");
+            } else if (day >= 23 && day <= 31) {
+                System.out.println("處女");
+            } else {
+                System.out.println("Invalid");
+            }
+        } else if (month == 9) {
+            if (day >= 1 && day <= 22) {
+                System.out.println("處女");
+            } else if (day >= 23 && day <= 30) {
+                System.out.println("天秤");
+            } else {
+                System.out.println("Invalid");
+            }
+        } else if (month == 10) {
+            if (day >= 1 && day <= 23) {
+                System.out.println("天秤");
+            } else if (day >= 24 && day <= 31) {
+                System.out.println("天蠍");
+            } else {
+                System.out.println("Invalid");
+            }
+        } else if (month == 11) {
+            if (day >= 1 && day <= 22) {
+                System.out.println("天蠍");
+            } else if (day >= 23 && day <= 30) {
+                System.out.println("射手");
+            } else {
+                System.out.println("Invalid");
+            }
+        } else if (month == 12) {
+            if (day >= 1 && day <= 21) {
+                System.out.println("射手");
+            } else if (day >= 22 && day <= 31) {
+                System.out.println("魔羯");
+            } else {
+                System.out.println("Invalid");
+            }
+        } else {
+            System.out.println("Invalid");
+        }
+    }
+}
