44bfd3a5c4d4fb87a53b4b4c1750f6ad649e52c1
young <young20050727@gmail.com>
2023-12-14 00:48:30 +0800

    7-4

--- Hw/7/Hw7_4.java
+++ Hw/7/Hw7_4.java
@@ -1,6 +1,20 @@
 public class Hw7_4 {
     public static void main(String[] args) {
-        // 寫一程式，輸入一字串，輸出有幾個英文字及幾個數字。寫一程式，判斷3x5矩陣中，共有幾列的資料列全為0。(提示：使用「continue標籤名稱;」)
-        
+        // 寫一程式，判斷3x5矩陣中，共有幾列的資料列全為0。(提示：使用「continue標籤名稱;」)
+        int[][] matrix = {
+            {0, 0, 1, 0, 2},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1}
+        };
+
+        int count = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (matrix[i][j] != 0) continue;
+                if (j == 4) count++;
+            }
+        }
+
+        System.out.println(count);
     }
 }
